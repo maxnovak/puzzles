fcb93b7156df5490d9bafbbe09551736e5609b82
maxnovak <novak.max@gmail.com>
2018-10-15 18:10:17 -0400

    Add description

--- WordFunnel.py
+++ WordFunnel.py
@@ -1,3 +1,7 @@
+'''This program will solve the problem:
+https://old.reddit.com/r/dailyprogrammer/comments/98ufvz/20180820_challenge_366_easy_word_funnel_1/
+'''
+
 def main():
     print("hello world")
 

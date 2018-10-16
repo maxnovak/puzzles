8065ec291c6b16399755623f75a69ab97155b4f5
Maxwell Novak <novak.max@gmail.com>
2018-10-16 14:14:48 -0400

    Add double arrow test

--- UpArrowNotation/UpArrowNotationTests.py
+++ UpArrowNotation/UpArrowNotationTests.py
@@ -2,8 +2,10 @@ import unittest
 import UpArrowNotation
 
 class TestIt(unittest.TestCase):
-	def test(self):
+	def testSingleArrow(self):
 		self.assertEqual(UpArrowNotation.arrow(2, 4), 16)
+	def testDoubleArrow(self):
+		self.assertEqual(UpArrowNotation.arrow(2, UpArrowNotation.arrow(2, 4)), 65536)
 
 if __name__ == '__main__':
 	unittest.main()

5effae34739f32a60448cfcfd0c5c73990d69034
Maxwell Novak <novak.max@gmail.com>
2018-10-16 14:22:03 -0400

    Add another test case

--- UpArrowNotation/UpArrowNotationTests.py
+++ UpArrowNotation/UpArrowNotationTests.py
@@ -4,6 +4,7 @@ from UpArrowNotation import arrow
 class TestIt(unittest.TestCase):
 	def testSingleArrow(self):
 		self.assertEqual(arrow(2, 4), 16)
+		self.assertEqual(arrow(2, 3), 8)
 	def testDoubleArrow(self):
 		self.assertEqual(arrow(2, arrow(2, 4)), 65536)
 	def testTripleArrow(self):

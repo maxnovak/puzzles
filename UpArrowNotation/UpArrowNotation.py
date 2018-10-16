0b3d39a9e7c17edbe71df9716b49ad71ee08ea4b
Maxwell Novak <novak.max@gmail.com>
2018-10-16 14:12:34 -0400

    Create method that that passes test

--- UpArrowNotation/UpArrowNotation.py
+++ UpArrowNotation/UpArrowNotation.py
@@ -1,1 +1,14 @@
 '''https://old.reddit.com/r/dailyprogrammer/comments/8xbxi9/20180709_challenge_365_easy_uparrow_notation/'''
+
+def main():
+	return arrow(2,4)
+
+def arrow(base_number, iterations):
+	result = 1
+	for count in range(iterations):
+		result = result * base_number
+	print(result)
+	return result
+
+if __name__ == '__main__':
+	main()

63fe5bf81228b0dc9e7dd41b1651cae252a63469
Maxwell Novak <novak.max@gmail.com>
2018-10-16 14:47:44 -0400

    Create simple dice roller per request from link

---
+++ DiceRoller/DiceRoller.py
@@ -0,0 +1,16 @@
+'''https://old.reddit.com/r/dailyprogrammer/comments/8s0cy1/20180618_challenge_364_easy_create_a_dice_roller/'''
+from random import *
+
+def main():
+	'''roll two six sided dice'''
+	print(roll(2,6))
+
+def roll(number, sides):
+	result = []
+	for number in range(number):
+		result.append(randrange(sides))
+
+	return result
+
+if __name__ == '__main__':
+    main()

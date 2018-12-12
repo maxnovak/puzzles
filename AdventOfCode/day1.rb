95b9afb7d7cb9b1f792b548098e58326f94d73af
Maxwell Novak <novak.max@gmail.com>
2018-12-12 16:12:04 -0500

    Read in frequencies to array and then sum them
    Doing this because in part 2 we find out that the values loop

--- AdventOfCode/day1.rb
+++ AdventOfCode/day1.rb
@@ -2,11 +2,16 @@
 #copied to text file day1_input.txt for ease of solving problem.
 
 current_frequency = 0
+read_in_frequencies = []
+
 File.open("day1_input.txt", "r") do |file|
 	file.each_line do |frequency_value|
-		current_frequency += frequency_value.to_i
+		read_in_frequencies.push(frequency_value.to_i)
 	end
 end
 
+for frequency in read_in_frequencies
+	current_frequency += frequency
+end
 #Part 1 solution
 puts current_frequency

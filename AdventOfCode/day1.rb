143fc785938397391050fc7d18cb655f7f871b9c
Maxwell Novak <novak.max@gmail.com>
2018-12-12 15:53:18 -0500

    Clean up variables to match theme of problem

--- AdventOfCode/day1.rb
+++ AdventOfCode/day1.rb
@@ -1,11 +1,12 @@
 #Input comes from https://adventofcode.com/2018/day/1/input
 #copied to text file day1_input.txt for ease of solving problem.
 
-sum = 0
-File.open("day1_input.txt", "r") do |f|
-	f.each_line do |line|
-		sum += line.to_i
+current_frequency = 0
+File.open("day1_input.txt", "r") do |file|
+	file.each_line do |frequency_value|
+		current_frequency += frequency_value.to_i
 	end
 end
 
-puts sum
+#Part 1 solution
+puts current_frequency

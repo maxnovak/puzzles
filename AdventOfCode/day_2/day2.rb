4d452c6d48d61dd8248e9af8559f5430491a3edd
Maxwell Novak <novak.max@gmail.com>
2018-12-13 14:31:25 -0500

    Part one solution

--- AdventOfCode/day_2/day2.rb
+++ AdventOfCode/day_2/day2.rb
@@ -11,4 +11,30 @@ File.open("day2_input.txt", "r") do |file|
 	end
 end
 
-p list_of_box_ids
+has_twos = false
+has_threes = false
+
+for box_id in list_of_box_ids
+	characters = box_id.split('')
+	for character in characters.uniq
+		if box_id.count(character) == 2
+			has_twos = true
+		elsif box_id.count(character) == 3
+			has_threes = true
+		end
+	end
+	if has_twos
+		two_counts += 1
+		has_twos = false
+	end
+	if has_threes
+		three_counts += 1
+		has_threes = false
+	end
+end
+
+puts "Two counts: #{two_counts}"
+puts "Three counts: #{three_counts}"
+
+#Part 1 solution
+puts "Checksum: #{two_counts * three_counts}"

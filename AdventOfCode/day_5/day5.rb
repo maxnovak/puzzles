fb4e7990733e562bde858cf34b1be60b7e11115a
Maxwell Novak <novak.max@gmail.com>
2018-12-16 18:45:52 -0500

    Break down the list into bite size pieces then rebuild it

--- AdventOfCode/day_5/day5.rb
+++ AdventOfCode/day_5/day5.rb
@@ -7,8 +7,18 @@ p comparisonAmount
 sequence = ""
 File.open("day5_input.txt", "r") do |file|
 	file.each_line do |data|
-		sequence = data
+		sequence = data.split('')
 	end
 end
 
-p sequence
+def doMagic(sequenceOfLetters)
+	if sequenceOfLetters.count > 2
+		left = doMagic(sequenceOfLetters[0..sequenceOfLetters.count/2])
+		right = doMagic(sequenceOfLetters[sequenceOfLetters.count/2+1..-1])
+		return left + right
+	else
+		return sequenceOfLetters
+	end
+end
+
+p doMagic(sequence) == sequence

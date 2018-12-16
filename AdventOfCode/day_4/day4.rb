e69d3ed409f9fe8a804f5c0076df4b240808ac64
Maxwell Novak <novak.max@gmail.com>
2018-12-16 13:14:57 -0500

    Load in events from input and sort them by date-time

---
+++ AdventOfCode/day_4/day4.rb
@@ -0,0 +1,16 @@
+#Input comes from https://adventofcode.com/2018/day/3/input
+#copied to text file day3_input.txt for ease of solving problem.
+
+require_relative "GuardEvent"
+require 'pp'
+
+events = []
+
+File.open("day4_input.txt", "r") do |file|
+	file.each_line do |claim_data|
+		data = claim_data.split(" ")
+		events.push(GuardEvent.new(data[0].tr("[",""), data[1].tr("]",""), data[2..-1]))
+	end
+end
+
+pp events.sort_by { |event| [event.date, event.time]  }

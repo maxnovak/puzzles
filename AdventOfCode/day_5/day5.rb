6dfdaa4473b9ac3075c24c18ed9ace62d024bc16
Maxwell Novak <novak.max@gmail.com>
2018-12-16 19:00:54 -0500

    Break the list down to single nodes

--- AdventOfCode/day_5/day5.rb
+++ AdventOfCode/day_5/day5.rb
@@ -12,7 +12,11 @@ File.open("day5_input.txt", "r") do |file|
 end
 
 def doMagic(sequenceOfLetters)
-	if sequenceOfLetters.count > 2
+	if sequenceOfLetters.count == 2
+		left = sequenceOfLetters[0]
+		right = sequenceOfLetters[1]
+		return [left] + [right]
+	elsif sequenceOfLetters.count > 2
 		left = doMagic(sequenceOfLetters[0..sequenceOfLetters.count/2])
 		right = doMagic(sequenceOfLetters[sequenceOfLetters.count/2+1..-1])
 		return left + right
@@ -21,4 +25,5 @@ def doMagic(sequenceOfLetters)
 	end
 end
 
+p sequence
 p doMagic(sequence) == sequence

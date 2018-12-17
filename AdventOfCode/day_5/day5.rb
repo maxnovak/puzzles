b2f81293d1b1f053ace73c8bfeca6643175e88ca
Maxwell Novak <novak.max@gmail.com>
2018-12-16 19:20:39 -0500

    Create comparison method and use it when breaking down list

--- AdventOfCode/day_5/day5.rb
+++ AdventOfCode/day_5/day5.rb
@@ -11,15 +11,25 @@ File.open("day5_input.txt", "r") do |file|
 	end
 end
 
+def checkValues(left, right)
+	difference = left.ord - right.ord
+	if difference.abs == $comparisonAmount
+		return []
+	end
+	return [left] + [right]
+end
+
 def doMagic(sequenceOfLetters)
 	if sequenceOfLetters.count == 2
 		left = sequenceOfLetters[0]
 		right = sequenceOfLetters[1]
-		return [left] + [right]
+		result = checkValues(left, right)
+		return result
 	elsif sequenceOfLetters.count > 2
 		left = doMagic(sequenceOfLetters[0..sequenceOfLetters.count/2])
 		right = doMagic(sequenceOfLetters[sequenceOfLetters.count/2+1..-1])
-		return left + right
+		result = checkValues(left.last, right.first)
+		return left[0..-2] + result + right[1..-1]
 	else
 		return sequenceOfLetters
 	end

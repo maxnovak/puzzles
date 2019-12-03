327c613426017b73587ad8ed4b70b08085154b34
Maxwell Novak <novak.max@gmail.com>
2019-12-03 00:10:53 -0500

    Read in input and return two arrays

--- AdventOfCode/2019/day-3/day-3.go
+++ AdventOfCode/2019/day-3/day-3.go
@@ -5,35 +5,46 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 	"strings"
 )
 
 func main() {
-
+	coord1, coord2 := readFile()
+	fmt.Println(coord1)
+	fmt.Println(coord2)
 }
 
-func readFile() []int{
+func readFile() ([]string, []string) {
 
-	file, err := os.Open("day-2-input.txt")
+	file, err := os.Open("day-3-input.txt")
     if err != nil {
         log.Fatal(err)
     }
     defer file.Close()
 
     scanner := bufio.NewScanner(file)
-    var intcodes []int
+    var coordinates1 []string
+    var coordinates2 []string
+    ind := 0
     for scanner.Scan() {
-    	input := strings.Split(scanner.Text(), ",")
-    	for _, i := range input {
-			number, _ := strconv.Atoi(i)
-        	intcodes = append(intcodes, number)
-    	}
+    	if (ind == 0) {
+	    	directions := strings.Split(scanner.Text(), ",")
+	    	for _, i := range directions {
+	        	coordinates1 = append(coordinates1, i)
+	    	}
+	    }
+	    if (ind == 1) {
+	    	directions := strings.Split(scanner.Text(), ",")
+	    	for _, i := range directions {
+	        	coordinates2 = append(coordinates2, i)
+	    	}	
+	    }
+	    ind++
     }
 
     if err := scanner.Err(); err != nil {
         log.Fatal(err)
     }
 
-    return intcodes
+    return coordinates1, coordinates2
 }

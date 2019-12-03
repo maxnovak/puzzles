8492919f01eed6d2db545a2fcb00e731f3fbf7d8
Maxwell Novak <novak.max@gmail.com>
2019-12-03 00:57:11 -0500

    Get ploted coordinates and return them

--- AdventOfCode/2019/day-3/day-3.go
+++ AdventOfCode/2019/day-3/day-3.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -12,22 +13,56 @@ func main() {
 	coord1, coord2 := readFile()
 	fmt.Println(coord1)
 	fmt.Println(coord2)
-	graph(coord1)
-	graph(coord2)
+	graph1 := graph(coord1)
+	graph2 := graph(coord2)
+	fmt.Println(graph1)
+	fmt.Println(graph2)
 }
 
-func graph(coord1 []string) {
+func graph(coord1 []string) [][]int{
+	var traversal [][]int
+	currentx := 1
+	currenty := 1
+	traversal = append(traversal, []int{currentx, currenty})
 	for _, location := range coord1 {
+		distance, _ := strconv.Atoi(location[1:len(location)])
+		var newx int
+		var newy int
 		if location[0] == 'R' {
-			fmt.Println("Right", location[1:len(location)])
+			distance = distance  + currentx + 1
+			for x := currentx+1; x < distance; x++ {
+				traversal = append(traversal, []int{x, currenty})
+				newx = x
+			}
+			currentx = newx
+			fmt.Println("Right", currentx)
 		} else if location[0] == 'L' {
-			fmt.Println("Left", location[1:len(location)])
+			distance = currentx - distance -1
+			for x := currentx-1; x > distance; x-- {
+				traversal = append(traversal, []int{x, currenty})
+				newx = x
+			}
+			currentx = newx
+			fmt.Println("Left", currentx, distance)
 		} else if location[0] == 'U' {
+			distance = distance  + currenty + 1
+			for y := currenty+1; y < distance; y++ {
+				traversal = append(traversal, []int{currentx, y})
+				newy = y
+			}
+			currenty = newy
 			fmt.Println("Up", location[1:len(location)])
 		} else if location[0] == 'D' {
+			distance = currenty - distance -1
+			for y := currenty-1; y > distance; y-- {
+				traversal = append(traversal, []int{currentx, y})
+				newy = y
+			}
+			currenty = newy
 			fmt.Println("Down", location[1:len(location)])
 		}
 	}
+	return traversal
 }
 
 func readFile() ([]string, []string) {

8213fb4146180147ec39e22c0b4c237ec88892d4
Maxwell Novak <novak.max@gmail.com>
2019-12-02 23:59:41 -0500

    Create template files for problem

---
+++ AdventOfCode/2019/day-3/day-3.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+}
+
+func readFile() []int{
+
+	file, err := os.Open("day-2-input.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    var intcodes []int
+    for scanner.Scan() {
+    	input := strings.Split(scanner.Text(), ",")
+    	for _, i := range input {
+			number, _ := strconv.Atoi(i)
+        	intcodes = append(intcodes, number)
+    	}
+    }
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+
+    return intcodes
+}

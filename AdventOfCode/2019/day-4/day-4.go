547340d62a12a84b7d75ebd07a956ac581e46017
Maxwell Novak <novak.max@gmail.com>
2019-12-04 08:03:38 -0500

    Check for full password range

--- AdventOfCode/2019/day-4/day-4.go
+++ AdventOfCode/2019/day-4/day-4.go
@@ -11,6 +11,16 @@ func main() {
 	fmt.Println("223450, ", checkIfValid("223450"))
 	fmt.Println("123789 ", checkIfValid("123789"))
 
+	validPasswords := 0
+
+	for password := 138307; password < 654504; password++ {
+		passwordString := strconv.Itoa(password)
+		if (checkIfValid(passwordString)) {
+			validPasswords++
+		}
+	}
+
+	fmt.Println(validPasswords)
 }
 
 func checkIfValid(password string) bool{

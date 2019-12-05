cab6448d4afc93adda9a36c0078c1293c8dc42e6
Maxwell Novak <novak.max@gmail.com>
2019-12-04 20:55:31 -0500

    Add part two logic in split out function

--- AdventOfCode/2019/day-4/day-4.go
+++ AdventOfCode/2019/day-4/day-4.go
@@ -15,15 +15,20 @@ func main() {
 	fmt.Println("111122 ", checkIfValid("111122"))
 
 	validPasswords := 0
+	validPart2Passwords := 0
 
 	for password := 138307; password < 654504; password++ {
 		passwordString := strconv.Itoa(password)
 		if (checkIfValid(passwordString)) {
 			validPasswords++
 		}
+		if (checkIfValid2(passwordString)) {
+			validPart2Passwords++
+		}
 	}
 
 	fmt.Println(validPasswords)
+	fmt.Println(validPart2Passwords)
 }
 
 func checkIfValid(password string) bool{
@@ -39,5 +44,32 @@ func checkIfValid(password string) bool{
 			mightBeValid = true
 		}
 	}
+	return mightBeValid
+}
+
+func checkIfValid2(password string) bool{
+	mightBeValid := false
+	currentRun := 1
+	for i :=0; i < len(password) -1; i++ {
+		firstChar := string(password[i])
+		secondChar := string(password[i+1])
+
+		if (firstChar > secondChar) {
+			return false
+		}
+		if (firstChar == secondChar) {
+			currentRun++
+		} else {
+			if currentRun == 2 {
+				mightBeValid = true
+			}
+			currentRun = 1
+		}
+	}
+
+	if (currentRun == 2) {
+		mightBeValid = true
+	}
+
 	return mightBeValid
 }

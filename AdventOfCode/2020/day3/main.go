9ac6463d4bcc999a033d782898a1956e071b4593
Maxwell Novak <novak.max@gmail.com>
2020-12-03 00:24:20 -0500

    Part 1 solution

--- AdventOfCode/2020/day3/main.go
+++ AdventOfCode/2020/day3/main.go
@@ -1,9 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
 	fmt.Println("This is day 3")
+	parsedMap := readFile()
+	fmt.Println(parsedMap)
+	result := findTreeCollisions(parsedMap)
+	fmt.Println(result)
+}
+
+func findTreeCollisions(geologyMap [][]bool) int {
+	x := 0
+	collisions := 0
+	for i, row := range geologyMap {
+		if i == 0 {
+			continue
+		}
+		x = (x + 3) % len(row)
+		if row[x] {
+			collisions++
+		}
+	}
+	return collisions
+}
+
+func readFile() [][]bool {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var geologyMap [][]bool
+	for scanner.Scan() {
+		var xCoords []bool
+		for _, letter := range scanner.Text() {
+			xCoords = append(xCoords, letter == '#')
+		}
+		geologyMap = append(geologyMap, xCoords)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return geologyMap
 }

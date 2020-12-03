07da390e98287ce511525054b794d339849396d5
Maxwell Novak <novak.max@gmail.com>
2020-12-03 00:39:01 -0500

    Add part 2 solution

--- AdventOfCode/2020/day3/main.go
+++ AdventOfCode/2020/day3/main.go
@@ -10,20 +10,27 @@ import (
 func main() {
 	fmt.Println("This is day 3")
 	parsedMap := readFile()
-	fmt.Println(parsedMap)
-	result := findTreeCollisions(parsedMap)
-	fmt.Println(result)
+	result11 := findTreeCollisions(parsedMap, 1, 1)
+	result31 := findTreeCollisions(parsedMap, 3, 1)
+	result51 := findTreeCollisions(parsedMap, 5, 1)
+	result71 := findTreeCollisions(parsedMap, 7, 1)
+	result12 := findTreeCollisions(parsedMap, 1, 2)
+	fmt.Printf("Right 1, down 1: %v\n", result11)
+	fmt.Printf("Right 3, down 1: %v\n", result31)
+	fmt.Printf("Right 5, down 1: %v\n", result51)
+	fmt.Printf("Right 7, down 1: %v\n", result71)
+	fmt.Printf("Right 1, down 2: %v\n", result12)
+
+	fmt.Printf("Answer to part 2: %v", result11*result12*result31*result51*result71)
 }
 
-func findTreeCollisions(geologyMap [][]bool) int {
+func findTreeCollisions(geologyMap [][]bool, rightAmount int, downAmount int) int {
 	x := 0
 	collisions := 0
-	for i, row := range geologyMap {
-		if i == 0 {
-			continue
-		}
-		x = (x + 3) % len(row)
-		if row[x] {
+	lengthOfData := len(geologyMap[0])
+	for y := downAmount; y < len(geologyMap); y = y + downAmount {
+		x = (x + rightAmount) % lengthOfData
+		if geologyMap[y][x] {
 			collisions++
 		}
 	}

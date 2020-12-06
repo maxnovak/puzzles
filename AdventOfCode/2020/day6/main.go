119e7387f820ef0b43ced082f79722b10661e73b
Maxwell Novak <novak.max@gmail.com>
2020-12-06 00:08:49 -0500

    Part 1 solution

--- AdventOfCode/2020/day6/main.go
+++ AdventOfCode/2020/day6/main.go
@@ -1,9 +1,48 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
 	fmt.Println("this is day 6")
+	forms := readFile()
+	sum := 0
+	for _, item := range forms {
+		sum += len(item)
+	}
+	fmt.Println(sum)
+}
+
+func readFile() []map[string]bool {
+
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var customsForms []map[string]bool
+	groupsForm := map[string]bool{}
+	for scanner.Scan() {
+		text := scanner.Text()
+		if text == "" {
+			customsForms = append(customsForms, groupsForm)
+			groupsForm = map[string]bool{}
+			continue
+		}
+		for _, item := range text {
+			groupsForm[string(item)] = true
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return customsForms
 }

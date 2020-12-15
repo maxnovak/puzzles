b00e649cf6a31de547de49d9af075e5b95dc4025
Maxwell Novak <novak.max@gmail.com>
2020-12-14 21:16:02 -0500

    Part 2 solution

--- AdventOfCode/2020/day9/main.go
+++ AdventOfCode/2020/day9/main.go
@@ -11,12 +11,38 @@ import (
 func main() {
 	fmt.Println("this is day 9")
 	numbers := readFile()
+	locationOfFailure := 0
 	for i := 25; i < len(numbers); i++ {
 		if !findIfSum(numbers[i-25:i], numbers[i]) {
 			fmt.Println("Part 1 solution", numbers[i])
+			locationOfFailure = i
 			break
 		}
 	}
+	for i := 0; i < locationOfFailure; i++ {
+		for j := i + 1; j < locationOfFailure; j++ {
+			sum, max, min := sumSlice(numbers[i:j])
+			if sum == numbers[locationOfFailure] {
+				fmt.Println("Part 2 solution", max+min)
+			}
+		}
+	}
+}
+
+func sumSlice(numbers []int) (int, int, int) {
+	result := 0
+	max := 0
+	min := numbers[0]
+	for _, number := range numbers {
+		if number > max {
+			max = number
+		}
+		if number < min {
+			min = number
+		}
+		result += number
+	}
+	return result, max, min
 }
 
 func findIfSum(numbers []int, sum int) bool {

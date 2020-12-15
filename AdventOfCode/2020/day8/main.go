ceb8102a98926bbac8a53c3fbba6f8ddadd0db4c
Maxwell Novak <novak.max@gmail.com>
2020-12-14 20:39:20 -0500

    Extract the operation logic into own block and solve part 2

--- AdventOfCode/2020/day8/main.go
+++ AdventOfCode/2020/day8/main.go
@@ -17,15 +17,47 @@ type operation struct {
 func main() {
 	fmt.Println("this is day 8")
 	ops := readFile()
+
+	accumulatedValue, _ := findLoopPoint(ops)
+
+	fmt.Println("Part 1 solution", accumulatedValue)
+
+	for i := 0; i < len(ops); i++ {
+		if ops[i].name == "nop" {
+			ops[i].name = "jmp"
+			accumulatedValue, foundIt := findLoopPoint(ops)
+			ops[i].name = "nop"
+			if foundIt {
+				fmt.Println("Part 2 solution:", accumulatedValue)
+				break
+			}
+		} else if ops[i].name == "jmp" {
+			ops[i].name = "nop"
+			accumulatedValue, foundIt := findLoopPoint(ops)
+			ops[i].name = "jmp"
+			if foundIt {
+				fmt.Println("Part 2 solution:", accumulatedValue)
+				break
+			}
+		}
+	}
+}
+
+func findLoopPoint(operations []operation) (int, bool) {
 	location := 0
 	visitedLocations := map[int]bool{}
 	accumulatedValue := 0
+	foundIt := false
 	for {
 		if visitedLocations[location] {
 			break
 		}
+		if location == len(operations) {
+			foundIt = true
+			break
+		}
 		visitedLocations[location] = true
-		op := ops[location]
+		op := operations[location]
 		if op.name == "nop" {
 			location++
 			continue
@@ -40,7 +72,7 @@ func main() {
 			continue
 		}
 	}
-	fmt.Println("Part 1 solution", accumulatedValue)
+	return accumulatedValue, foundIt
 }
 
 func readFile() []operation {

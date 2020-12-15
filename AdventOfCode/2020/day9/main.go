af054c1189e4176153d7ca920bbc0183de281f05
Maxwell Novak <novak.max@gmail.com>
2020-12-14 21:01:58 -0500

    Part 1 solution

--- AdventOfCode/2020/day9/main.go
+++ AdventOfCode/2020/day9/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
 func main() {
 	fmt.Println("this is day 9")
+	numbers := readFile()
+	for i := 25; i < len(numbers); i++ {
+		if !findIfSum(numbers[i-25:i], numbers[i]) {
+			fmt.Println("Part 1 solution", numbers[i])
+			break
+		}
+	}
+}
+
+func findIfSum(numbers []int, sum int) bool {
+	for i := 0; i < len(numbers); i++ {
+		for j := i + 1; j < len(numbers); j++ {
+			if numbers[i]+numbers[j] == sum {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func readFile() []int {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var numbers []int
+	for scanner.Scan() {
+		text := scanner.Text()
+		number, _ := strconv.Atoi(text)
+		numbers = append(numbers, number)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return numbers
 }

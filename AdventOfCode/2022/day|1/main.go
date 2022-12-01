a909fd11e5606383cedadc6072b4836a1f0fbe5c
Maxwell Novak <max.novak@policygenius.com>
2022-12-01 10:42:04 -0500

    Simplify solution and solve part 2

--- AdventOfCode/2022/day|1/main.go
+++ AdventOfCode/2022/day|1/main.go
@@ -5,30 +5,23 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 )
-
-type data struct {
-	elfNumber     int
-	totalCalories int
-}
 
 func main() {
 	fmt.Println("This is day 1")
 	data := readFile()
+	sort.Ints(data)
 
-	maxCalories := 0
-	for index, item := range data {
-		if index == 0 || item.totalCalories > maxCalories {
-			maxCalories = item.totalCalories
-		}
-	}
+	fmt.Printf("Most calories: %v", data[len(data)-1])
+	fmt.Printf("Second most calories: %v", data[len(data)-2])
+	fmt.Printf("Third most calories: %v", data[len(data)-3])
+	fmt.Printf("Sum of calories: %v", data[len(data)-1]+data[len(data)-2]+data[len(data)-3])
 
-	fmt.Printf("Most calories: %v", maxCalories)
 }
 
-func readFile() []data {
-
+func readFile() []int {
 	file, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -36,17 +29,12 @@ func readFile() []data {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	var elfList []data
+	var elfList []int
 	totalCal := 0
-	elfNumber := 1
 	for scanner.Scan() {
 		output := scanner.Text()
 		if output == "" {
-			elfList = append(elfList, data{
-				elfNumber:     elfNumber,
-				totalCalories: totalCal,
-			})
-			elfNumber++
+			elfList = append(elfList, totalCal)
 			totalCal = 0
 		} else {
 			parsedValue, _ := strconv.Atoi(output)

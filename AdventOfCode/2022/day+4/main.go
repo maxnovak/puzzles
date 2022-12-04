d9511094b7c808fe1ebf63d51a089e4eb615f60d
Maxwell Novak <novak.max@gmail.com>
2022-12-04 00:48:51 -0500

    Day 4 part 1 solution for 2022

---
+++ AdventOfCode/2022/day+4/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Data struct {
+	RangeOneStart int
+	RangeOneEnd   int
+	RangeTwoStart int
+	RangeTwoEnd   int
+}
+
+func main() {
+	fmt.Println("This is day 4")
+	pairsRanges := readFile()
+	count := 0
+	for _, pair := range pairsRanges {
+		if pair.RangeOneStart <= pair.RangeTwoStart &&
+			pair.RangeTwoEnd <= pair.RangeOneEnd {
+			count++
+		} else if pair.RangeTwoStart <= pair.RangeOneStart &&
+			pair.RangeOneEnd <= pair.RangeTwoEnd {
+			count++
+		}
+	}
+	fmt.Println(count)
+}
+
+func readFile() []Data {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var data []Data
+	for scanner.Scan() {
+		pairs := strings.Split(scanner.Text(), ",")
+		first := strings.Split(pairs[0], "-")
+		second := strings.Split(pairs[1], "-")
+		rangeOneStart, _ := strconv.Atoi(first[0])
+		rangeOneEnd, _ := strconv.Atoi(first[1])
+		rangeTwoStart, _ := strconv.Atoi(second[0])
+		rangeTwoEnd, _ := strconv.Atoi(second[1])
+		data = append(data, Data{
+			RangeOneStart: rangeOneStart,
+			RangeOneEnd:   rangeOneEnd,
+			RangeTwoStart: rangeTwoStart,
+			RangeTwoEnd:   rangeTwoEnd,
+		})
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return data
+}

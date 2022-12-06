4d832aeda51219cacf9f7e9b26773d3ae482a6fb
Maxwell Novak <novak.max@gmail.com>
2022-12-06 16:59:52 -0500

    Add part 2 solution

--- AdventOfCode/2022/day+6/main.go
+++ AdventOfCode/2022/day+6/main.go
@@ -11,8 +11,10 @@ import (
 
 func main() {
 	data := readFile()
-	firstMarker := findFirstMarker(data)
+	firstMarker := findFirstMarker4(data)
+	firstMarker14 := findFirstMarker14(data)
 	fmt.Printf("Part 1 First Marker: %v\n", firstMarker)
+	fmt.Printf("Part 1 First Marker: %v\n", firstMarker14)
 }
 
 func readFile() string {
@@ -35,7 +37,7 @@ func readFile() string {
 	return dataStream
 }
 
-func findFirstMarker(data string) int {
+func findFirstMarker4(data string) int {
 	for i := 4; i < len(data); i++ {
 		set := append([]string{}, strings.Split(data[i-4:i], "")...)
 		sort.Strings(set)
@@ -47,6 +49,26 @@ func findFirstMarker(data string) int {
 				break
 			}
 			if j == 3 {
+				return i
+			}
+		}
+	}
+
+	return 0
+}
+
+func findFirstMarker14(data string) int {
+	for i := 14; i < len(data); i++ {
+		set := append([]string{}, strings.Split(data[i-14:i], "")...)
+		sort.Strings(set)
+		for j, character := range set {
+			if j == 0 {
+				continue
+			}
+			if set[j-1] == character {
+				break
+			}
+			if j == 13 {
 				return i
 			}
 		}

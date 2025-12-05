ce8594daab6fe9c7a6557782e60d62851708d0f6
Maxwell Novak <mnovak@movableink.com>
2025-12-05 10:47:50 -0500

    Day 4 part 2 solution

--- AdventOfCode/2025/day04/solution.js
+++ AdventOfCode/2025/day04/solution.js
@@ -24,32 +24,49 @@ const searchCoords = [
  [1,-1],  [1,0],  [1,1]
 ];
 
-let accessable = 0;
-
-for (let y in warehouse) {
-  for (let x in warehouse[y]) {
-    if (warehouse[y][x] === "space") {
-      continue;
-    }
-
-    let filledSpaceCount = 0;
-    for (let coord of searchCoords) {
-      const searchY = parseInt(y) + coord[0];
-      const searchX = parseInt(x) + coord[1];
-      if (!warehouse[searchY]) {
+const getAccessableRolls = (warehouse) => {
+  let accessable = 0;
+  const spacesToClear = []
+  for (let y in warehouse) {
+    for (let x in warehouse[y]) {
+      if (warehouse[y][x] === "space") {
         continue;
       }
-      if (!warehouse[searchY][searchX]){
-        continue;
+
+      let filledSpaceCount = 0;
+      for (let coord of searchCoords) {
+        const searchY = parseInt(y) + coord[0];
+        const searchX = parseInt(x) + coord[1];
+        if (!warehouse[searchY]) {
+          continue;
+        }
+        if (!warehouse[searchY][searchX]){
+          continue;
+        }
+        if (warehouse[searchY][searchX] === "paper") {
+          filledSpaceCount++;
+        }
       }
-      if (warehouse[searchY][searchX] === "paper") {
-        filledSpaceCount++;
+      if (filledSpaceCount < 4) {
+        accessable++;
+        spacesToClear.push([y,x])
       }
-    }
-    if (filledSpaceCount < 4) {
-      accessable++;
     }
   }
+  return [accessable, spacesToClear];
 }
 
-console.log(accessable);
+console.log("Part 1 solution: ", getAccessableRolls(warehouse)[0]);
+let freedSpaces = 0
+let totalFreedSpaces = 0
+do {
+  response = getAccessableRolls(warehouse);
+  freedSpaces = response[0];
+  const spacesToClear = response[1];
+  for (let space of spacesToClear) {
+    warehouse[space[0]][space[1]] = "space";
+  }
+  totalFreedSpaces += freedSpaces;
+} while(freedSpaces > 0)
+
+console.log("Part 2 solution: ", totalFreedSpaces);
